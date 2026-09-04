c31cbb66069587147cbb8c6cd1815126031bca3b
rebeccahsiang <80782748+rebeccahsiang@users.noreply.github.com>
2026-09-04 14:08:28 +0000

    Deploying to gh-pages from @ rebeccahsiang/KQC-Frontend@a0711ed649d7bb6a8220dbe155d518c3441cc689 🚀

---
+++ assets/ContactInquiriesView-CODCYmJ7.js
@@ -0,0 +1,185 @@
+import{a as e,t}from"./axios-DDpStBLg.js";import{A as n,C as r,D as ee,Dt as i,F as a,G as o,I as s,M as c,N as l,P as u,W as d,at as f,c as p,et as m,g as h,h as g,l as _,m as v,o as y,r as b,s as x,u as S,wt as C}from"./runtime-core.esm-bundler-CJwJIixi.js";import{S as w,n as T,t as E}from"./basecomponent-TGs4kQcs.js";import{G as D,Q as O,a as k}from"./ripple-98AHvk_w.js";import{n as A,t as j}from"./portal-BSljaLEu.js";import{D as M,E as N,I as P,O as F,T as I,g as L,p as R}from"./index-C38R1eM6.js";import{n as z,t as B}from"./contactServices-DwJuMJv8.js";import{t as V}from"./paginator-7_63ImGn.js";import{t as H}from"./select-UZdrEXy0.js";import{i as U,n as W,t as G}from"./tag-rwnyOAT9.js";var K=T.extend({name:`drawer`,style:`
+    .p-drawer {
+        display: flex;
+        flex-direction: column;
+        transform: translate3d(0px, 0px, 0px);
+        position: relative;
+        transition: transform 0.3s;
+        background: dt('drawer.background');
+        color: dt('drawer.color');
+        border-style: solid;
+        border-color: dt('drawer.border.color');
+        box-shadow: dt('drawer.shadow');
+    }
+
+    .p-drawer-content {
+        overflow-y: auto;
+        flex-grow: 1;
+        padding: dt('drawer.content.padding');
+    }
+
+    .p-drawer-header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-shrink: 0;
+        padding: dt('drawer.header.padding');
+    }
+
+    .p-drawer-footer {
+        padding: dt('drawer.footer.padding');
+    }
+
+    .p-drawer-title {
+        font-weight: dt('drawer.title.font.weight');
+        font-size: dt('drawer.title.font.size');
+    }
+
+    .p-drawer-full .p-drawer {
+        transition: none;
+        transform: none;
+        width: 100vw !important;
+        height: 100vh !important;
+        max-height: 100%;
+        top: 0px !important;
+        left: 0px !important;
+        border-width: 1px;
+    }
+
+    .p-drawer-left .p-drawer-enter-active {
+        animation: p-animate-drawer-enter-left 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+    .p-drawer-left .p-drawer-leave-active {
+        animation: p-animate-drawer-leave-left 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+
+    .p-drawer-right .p-drawer-enter-active {
+        animation: p-animate-drawer-enter-right 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+    .p-drawer-right .p-drawer-leave-active {
+        animation: p-animate-drawer-leave-right 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+
+    .p-drawer-top .p-drawer-enter-active {
+        animation: p-animate-drawer-enter-top 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+    .p-drawer-top .p-drawer-leave-active {
+        animation: p-animate-drawer-leave-top 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+
+    .p-drawer-bottom .p-drawer-enter-active {
+        animation: p-animate-drawer-enter-bottom 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+    .p-drawer-bottom .p-drawer-leave-active {
+        animation: p-animate-drawer-leave-bottom 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+
+    .p-drawer-full .p-drawer-enter-active {
+        animation: p-animate-drawer-enter-full 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+    .p-drawer-full .p-drawer-leave-active {
+        animation: p-animate-drawer-leave-full 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+    }
+    
+    .p-drawer-left .p-drawer {
+        width: 20rem;
+        height: 100%;
+        border-inline-end-width: 1px;
+    }
+
+    .p-drawer-right .p-drawer {
+        width: 20rem;
+        height: 100%;
+        border-inline-start-width: 1px;
+    }
+
+    .p-drawer-top .p-drawer {
+        height: 10rem;
+        width: 100%;
+        border-block-end-width: 1px;
+    }
+
+    .p-drawer-bottom .p-drawer {
+        height: 10rem;
+        width: 100%;
+        border-block-start-width: 1px;
+    }
+
+    .p-drawer-left .p-drawer-content,
+    .p-drawer-right .p-drawer-content,
+    .p-drawer-top .p-drawer-content,
+    .p-drawer-bottom .p-drawer-content {
+        width: 100%;
+        height: 100%;
+    }
+
+    .p-drawer-open {
+        display: flex;
+    }
+
+    .p-drawer-mask:dir(rtl) {
+        flex-direction: row-reverse;
+    }
+
+    @keyframes p-animate-drawer-enter-left {
+        from {
+            transform: translate3d(-100%, 0px, 0px);
+        }
+    }
+
+    @keyframes p-animate-drawer-leave-left {
+        to {
+            transform: translate3d(-100%, 0px, 0px);
+        }
+    }
+
+    @keyframes p-animate-drawer-enter-right {
+        from {
+            transform: translate3d(100%, 0px, 0px);
+        }
+    }
+
+    @keyframes p-animate-drawer-leave-right {
+        to {
+            transform: translate3d(100%, 0px, 0px);
+        }
+    }
+
+    @keyframes p-animate-drawer-enter-top {
+        from {
+            transform: translate3d(0px, -100%, 0px);
+        }
+    }
+
+    @keyframes p-animate-drawer-leave-top {
+        to {
+            transform: translate3d(0px, -100%, 0px);
+        }
+    }
+
+    @keyframes p-animate-drawer-enter-bottom {
+        from {
+            transform: translate3d(0px, 100%, 0px);
+        }
+    }
+
+    @keyframes p-animate-drawer-leave-bottom {
+        to {
+            transform: translate3d(0px, 100%, 0px);
+        }
+    }
+
+    @keyframes p-animate-drawer-enter-full {
+        from {
+            opacity: 0;
+            transform: scale(0.93);
+        }
+    }
+
+    @keyframes p-animate-drawer-leave-full {
+        to {
+            opacity: 0;
+            transform: scale(0.93);
+        }
+    }
+`,classes:{mask:function(e){var t=e.instance,n=e.props,r=[`left`,`right`,`top`,`bottom`].find(function(e){return e===n.position});return[`p-drawer-mask`,{"p-overlay-mask p-overlay-mask-enter-active":n.modal,"p-drawer-open":t.containerVisible,"p-drawer-full":t.fullScreen},r?`p-drawer-${r}`:``]},root:function(e){return[`p-drawer p-component`,{"p-drawer-full":e.instance.fullScreen}]},header:`p-drawer-header`,title:`p-drawer-title`,pcCloseButton:`p-drawer-close-button`,content:`p-drawer-content`,footer:`p-drawer-footer`},inlineStyles:{mask:function(e){var t=e.position,n=e.modal;return{position:`fixed`,height:`100%`,width:`100%`,left:0,top:0,display:`flex`,justifyContent:t===`left`?`flex-start`:t===`right`?`flex-end`:`center`,alignItems:t===`top`?`flex-start`:t===`bottom`?`flex-end`:`center`,pointerEvents:n?`auto`:`none`}},root:{pointerEvents:`auto`}}}),q={name:`BaseDrawer`,extends:E,props:{visible:{type:Boolean,default:!1},position:{type:String,default:`left`},header:{type:null,default:null},baseZIndex:{type:Number,default:0},autoZIndex:{type:Boolean,default:!0},dismissable:{type:Boolean,default:!0},showCloseIcon:{type:Boolean,default:!0},closeButtonProps:{type:Object,default:function(){return{severity:`secondary`,text:!0,rounded:!0}}},closeIcon:{type:String,default:void 0},modal:{type:Boolean,default:!0},blockScroll:{type:Boolean,default:!1},closeOnEscape:{type:Boolean,default:!0}},style:K,provide:function(){return{$pcDrawer:this,$parentInstance:this}}};function J(e){"@babel/helpers - typeof";return J=typeof Symbol==`function`&&typeof Symbol.iterator==`symbol`?function(e){return typeof e}:function(e){return e&&typeof Symbol==`function`&&e.constructor===Symbol&&e!==Symbol.prototype?`symbol`:typeof e},J(e)}function Y(e,t,n){return(t=X(t))in e?Object.defineProperty(e,t,{value:n,enumerable:!0,configurable:!0,writable:!0}):e[t]=n,e}function X(e){var t=te(e,`string`);return J(t)==`symbol`?t:t+``}function te(e,t){if(J(e)!=`object`||!e)return e;var n=e[Symbol.toPrimitive];if(n!==void 0){var r=n.call(e,t);if(J(r)!=`object`)return r;throw TypeError(`@@toPrimitive must return a primitive value.`)}return(t===`string`?String:Number)(e)}var Z={name:`Drawer`,extends:q,inheritAttrs:!1,emits:[`update:visible`,`show`,`after-show`,`hide`,`after-hide`,`before-hide`],data:function(){return{containerVisible:this.visible}},container:null,mask:null,content:null,headerContainer:null,footerContainer:null,closeButton:null,outsideClickListener:null,documentKeydownListener:null,watch:{dismissable:function(e){e&&!this.modal?this.bindOutsideClickListener():this.unbindOutsideClickListener()}},updated:function(){this.visible&&(this.containerVisible=this.visible)},beforeUnmount:function(){this.disableDocumentSettings(),this.mask&&this.autoZIndex&&A.clear(this.mask),this.container=null,this.mask=null},methods:{hide:function(){this.$emit(`update:visible`,!1)},onEnter:function(){this.$emit(`show`),this.focus(),this.bindDocumentKeyDownListener(),this.autoZIndex&&A.set(`modal`,this.mask,this.baseZIndex||this.$primevue.config.zIndex.modal)},onAfterEnter:function(){this.enableDocumentSettings(),this.$emit(`after-show`)},onBeforeLeave:function(){this.modal&&!this.isUnstyled&&D(this.mask,`p-overlay-mask-leave-active`),this.$emit(`before-hide`)},onLeave:function(){this.$emit(`hide`)},onAfterLeave:function(){this.autoZIndex&&A.clear(this.mask),this.unbindDocumentKeyDownListener(),this.containerVisible=!1,this.disableDocumentSettings(),this.$emit(`after-hide`)},onMaskClick:function(e){this.dismissable&&this.modal&&this.mask===e.target&&this.hide()},focus:function(){var e=function(e){return e&&e.querySelector(`[autofocus]`)},t=this.$slots.header&&e(this.headerContainer);t||(t=this.$slots.default&&e(this.container),t||(t=this.$slots.footer&&e(this.footerContainer),t||=this.closeButton)),t&&O(t)},enableDocumentSettings:function(){this.dismissable&&!this.modal&&this.bindOutsideClickListener(),this.blockScroll&&I()},disableDocumentSettings:function(){this.unbindOutsideClickListener(),this.blockScroll&&N()},onKeydown:function(e){e.code===`Escape`&&this.closeOnEscape&&this.hide()},containerRef:function(e){this.container=e},maskRef:function(e){this.mask=e},contentRef:function(e){this.content=e},headerContainerRef:function(e){this.headerContainer=e},footerContainerRef:function(e){this.footerContainer=e},closeButtonRef:function(e){this.closeButton=e?e.$el:void 0},bindDocumentKeyDownListener:function(){this.documentKeydownListener||(this.documentKeydownListener=this.onKeydown,document.addEventListener(`keydown`,this.documentKeydownListener))},unbindDocumentKeyDownListener:function(){this.documentKeydownListener&&=(document.removeEventListener(`keydown`,this.documentKeydownListener),null)},bindOutsideClickListener:function(){var e=this;this.outsideClickListener||(this.outsideClickListener=function(t){e.isOutsideClicked(t)&&e.hide()},document.addEventListener(`click`,this.outsideClickListener,!0))},unbindOutsideClickListener:function(){this.outsideClickListener&&=(document.removeEventListener(`click`,this.outsideClickListener,!0),null)},isOutsideClicked:function(e){return this.container&&!this.container.contains(e.target)}},computed:{fullScreen:function(){return this.position===`full`},closeAriaLabel:function(){return this.$primevue.config.locale.aria?this.$primevue.config.locale.aria.close:void 0},dataP:function(){return w(Y(Y(Y({"full-screen":this.position===`full`},this.position,this.position),`open`,this.containerVisible),`modal`,this.modal))}},directives:{focustrap:M},components:{Button:F,Portal:j,TimesIcon:k}},ne=[`data-p`],re=[`role`,`aria-modal`,`data-p`];function ie(e,t,ee,c,f,m){var h=u(`Button`),v=u(`Portal`),y=a(`focustrap`);return n(),p(v,null,{default:d(function(){return[f.containerVisible?(n(),S(`div`,r({key:0,ref:m.maskRef,onMousedown:t[0]||=function(){return m.onMaskClick&&m.onMaskClick.apply(m,arguments)},class:e.cx(`mask`),style:e.sx(`mask`,!0,{position:e.position,modal:e.modal}),"data-p":m.dataP},e.ptm(`mask`)),[g(P,r({name:`p-drawer`,onEnter:m.onEnter,onAfterEnter:m.onAfterEnter,onBeforeLeave:m.onBeforeLeave,onLeave:m.onLeave,onAfterLeave:m.onAfterLeave,appear:``},e.ptm(`transition`)),{default:d(function(){return[e.visible?o((n(),S(`div`,r({key:0,ref:m.containerRef,class:e.cx(`root`),style:e.sx(`root`),role:e.modal?`dialog`:`complementary`,"aria-modal":e.modal?!0:void 0,"data-p":m.dataP},e.ptmi(`root`)),[e.$slots.container?l(e.$slots,`container`,{key:0,closeCallback:m.hide}):(n(),S(b,{key:1},[x(`div`,r({ref:m.headerContainerRef,class:e.cx(`header`)},e.ptm(`header`)),[l(e.$slots,`header`,{class:C(e.cx(`title`))},function(){return[e.header?(n(),S(`div`,r({key:0,class:e.cx(`title`)},e.ptm(`title`)),i(e.header),17)):_(``,!0)]}),e.showCloseIcon?l(e.$slots,`closebutton`,{key:0,closeCallback:m.hide},function(){return[g(h,r({ref:m.closeButtonRef,type:`button`,class:e.cx(`pcCloseButton`),"aria-label":m.closeAriaLabel,unstyled:e.unstyled,onClick:m.hide},e.closeButtonProps,{pt:e.ptm(`pcCloseButton`),"data-pc-group-section":`iconcontainer`}),{icon:d(function(t){return[l(e.$slots,`closeicon`,{},function(){return[(n(),p(s(e.closeIcon?`span`:`TimesIcon`),r({class:[e.closeIcon,t.class]},e.ptm(`pcCloseButton`).icon),null,16,[`class`]))]})]}),_:3},16,[`class`,`aria-label`,`unstyled`,`onClick`,`pt`])]}):_(``,!0)],16),x(`div`,r({ref:m.contentRef,class:e.cx(`content`)},e.ptm(`content`)),[l(e.$slots,`default`)],16),e.$slots.footer?(n(),S(`div`,r({key:0,ref:m.footerContainerRef,class:e.cx(`footer`)},e.ptm(`footer`)),[l(e.$slots,`footer`)],16)):_(``,!0)],64))],16,re)),[[y]]):_(``,!0)]}),_:3},16,[`onEnter`,`onAfterEnter`,`onBeforeLeave`,`onLeave`,`onAfterLeave`])],16,ne)):_(``,!0)]}),_:3})}Z.render=ie;var Q={list:(e=1,n=25)=>t.get(`/v1/admin/contact-inquiries`,{params:{page:e,limit:n}}),detail:e=>t.get(`/v1/admin/contact-inquiries/${encodeURIComponent(e)}`),updateStatus:(e,n)=>t.patch(`/v1/admin/contact-inquiries/${encodeURIComponent(e)}/status`,{status:n})},ae={class:`contact-inquiries-admin`},oe={key:0},se={class:`row-actions`},ce={key:0,class:`drawer-state`},le={key:1,class:`detail-content`},ue=[`href`],de=[`href`],fe={key:1},pe={class:`service-tags`},$=25,me=R(h({__name:`ContactInquiriesView`,setup(t){let r=[{value:`PENDING`,label:`待處理`},{value:`IN_PROGRESS`,label:`處理中`},{value:`COMPLETED`,label:`已完成`},{value:`CLOSED`,label:`已關閉`}],a=new Map(B.flatMap(({services:e})=>e.map(({code:e,label:t})=>[e,t]))),o=e=>r.find(({value:t})=>t===e)?.label||e,s=e=>({PENDING:`warn`,IN_PROGRESS:`info`,COMPLETED:`success`,CLOSED:`secondary`})[e],l=e=>e?new Intl.DateTimeFormat(`zh-TW`,{dateStyle:`medium`,timeStyle:`short`}).format(new Date(e)):`—`,u=t=>e(t)&&(t.response?.data)?.error?.message||`聯絡諮詢操作失敗。`,h=m([]),C=m({page:1,limit:$,total:0,totalPages:0}),w=m(!1),T=m(``),E=m(!1),D=m(!1),O=m(null),k=m(``),A=y(()=>O.value?z(O.value.serviceTypes):[]),j=y(()=>new Set(A.value.map(({key:e})=>e))),M=y(()=>Object.entries(O.value?.answers||{}).filter(([e])=>!j.value.has(e))),N=async(e=C.value.page)=>{w.value=!0,T.value=``;try{let t=await Q.list(e,$);h.value=t.data.inquiries,C.value=t.data.pagination}catch(e){T.value=u(e)}finally{w.value=!1}},P=async e=>{E.value=!0,D.value=!0,O.value=null,T.value=``;try{O.value=(await Q.detail(e.id)).data.inquiry}catch(e){T.value=u(e),E.value=!1}finally{D.value=!1}},I=async(e,t)=>{if(!(k.value||e.status===t)){k.value=e.id,T.value=``;try{let n=(await Q.updateStatus(e.id,t)).data.inquiry,r=h.value.findIndex(({id:t})=>t===e.id);r>=0&&(h.value[r]={...h.value[r],status:n.status,assignedTo:n.assignedTo}),O.value?.id===e.id&&(O.value=n)}catch(e){T.value=u(e)}finally{k.value=``}}},R=e=>({primary:a.get(e[0])||e[0],extra:Math.max(0,e.length-1)});return ee(()=>N()),(e,t)=>(n(),S(`section`,ae,[t[21]||=x(`header`,null,[x(`div`,null,[x(`p`,{class:`eyebrow`},`訊息管理`),x(`h1`,null,`聯絡我們諮詢`),x(`p`,null,`檢視公開聯絡表單，並維護目前處理狀態。`)])],-1),T.value?(n(),p(f(L),{key:0,severity:`error`,closable:!1},{default:d(()=>[v(i(T.value),1)]),_:1})):_(``,!0),g(f(W),{value:h.value,loading:w.value,"striped-rows":``,"empty-message":`目前沒有聯絡諮詢。`,"responsive-layout":`scroll`},{default:d(()=>[g(f(U),{header:`送出時間`},{body:d(({data:e})=>[v(i(l(e.createdAt)),1)]),_:1}),g(f(U),{header:`姓名／公司`},{body:d(({data:e})=>[x(`strong`,null,i(e.name),1),x(`small`,null,i(e.companyName||`未填公司`),1)]),_:1}),g(f(U),{header:`服務需求`},{body:d(({data:e})=>[x(`span`,null,i(R(e.serviceTypes).primary),1),R(e.serviceTypes).extra?(n(),S(`small`,oe,`+`+i(R(e.serviceTypes).extra),1)):_(``,!0)]),_:1}),g(f(U),{field:`contactUrgency`,header:`急迫度`},{body:d(({data:e})=>[v(i(e.contactUrgency||`—`),1)]),_:1}),g(f(U),{header:`狀態`},{body:d(({data:e})=>[g(f(H),{"model-value":e.status,options:r,"option-label":`label`,"option-value":`value`,"aria-label":`更新處理狀態`,disabled:!!k.value,"onUpdate:modelValue":t=>I(e,t)},null,8,[`model-value`,`disabled`,`onUpdate:modelValue`])]),_:1}),g(f(U),{header:`負責人`},{body:d(({data:e})=>[x(`span`,null,i(e.assignedTo||`未指派`),1)]),_:1}),g(f(U),{header:`操作`},{body:d(({data:e})=>[x(`div`,se,[g(f(F),{label:`查看`,icon:`pi pi-eye`,size:`small`,text:``,"aria-label":`查看詳細資料`,onClick:t=>P(e)},null,8,[`onClick`]),g(f(F),{label:`指派`,icon:`pi pi-user-plus`,severity:`secondary`,text:``,disabled:``,title:`規劃中`,"aria-label":`指派負責人（規劃中）`})])]),_:1})]),_:1},8,[`value`,`loading`]),C.value.totalPages>1?(n(),p(f(V),{key:1,first:(C.value.page-1)*C.value.limit,rows:C.value.limit,"total-records":C.value.total,onPage:t[0]||=e=>N(e.page+1)},null,8,[`first`,`rows`,`total-records`])):_(``,!0),g(f(Z),{visible:E.value,"onUpdate:visible":t[1]||=e=>E.value=e,header:`聯絡諮詢詳細資料`,position:`right`,class:`contact-inquiry-drawer`},{default:d(()=>[D.value?(n(),S(`div`,ce,`載入詳細資料中…`)):O.value?(n(),S(`article`,le,[x(`section`,null,[t[5]||=x(`h2`,null,`諮詢資料`,-1),x(`dl`,null,[x(`div`,null,[t[2]||=x(`dt`,null,`諮詢編號`,-1),x(`dd`,null,i(O.value.inquiryNo),1)]),x(`div`,null,[t[3]||=x(`dt`,null,`送出時間`,-1),x(`dd`,null,i(l(O.value.createdAt)),1)]),x(`div`,null,[t[4]||=x(`dt`,null,`狀態`,-1),x(`dd`,null,[g(f(G),{value:o(O.value.status),severity:s(O.value.status)},null,8,[`value`,`severity`])])])])]),x(`section`,null,[t[10]||=x(`h2`,null,`聯絡人`,-1),x(`dl`,null,[x(`div`,null,[t[6]||=x(`dt`,null,`稱呼`,-1),x(`dd`,null,i(O.value.profile.salutation),1)]),x(`div`,null,[t[7]||=x(`dt`,null,`姓名`,-1),x(`dd`,null,i(O.value.profile.name),1)]),x(`div`,null,[t[8]||=x(`dt`,null,`公司名稱`,-1),x(`dd`,null,i(O.value.profile.companyName||`—`),1)]),x(`div`,null,[t[9]||=x(`dt`,null,`職稱`,-1),x(`dd`,null,i(O.value.profile.jobTitle||`—`),1)])])]),x(`section`,null,[t[14]||=x(`h2`,null,`聯絡方式`,-1),x(`dl`,null,[x(`div`,null,[t[11]||=x(`dt`,null,`手機`,-1),x(`dd`,null,[x(`a`,{href:`tel:${O.value.profile.mobile}`},i(O.value.profile.mobile),9,ue)])]),x(`div`,null,[t[12]||=x(`dt`,null,`Email`,-1),x(`dd`,null,[O.value.profile.email?(n(),S(`a`,{key:0,href:`mailto:${O.value.profile.email}`},i(O.value.profile.email),9,de)):(n(),S(`span`,fe,`—`))])]),x(`div`,null,[t[13]||=x(`dt`,null,`LINE ID`,-1),x(`dd`,null,i(O.value.profile.lineId||`—`),1)])])]),x(`section`,null,[t[15]||=x(`h2`,null,`需求服務`,-1),x(`div`,pe,[(n(!0),S(b,null,c(O.value.serviceTypes,e=>(n(),p(f(G),{key:e,value:f(a).get(e)||e,severity:`info`},null,8,[`value`]))),128))])]),x(`section`,null,[t[16]||=x(`h2`,null,`需求內容`,-1),x(`dl`,null,[(n(!0),S(b,null,c(A.value,e=>(n(),S(`div`,{key:e.key},[x(`dt`,null,i(e.label),1),x(`dd`,null,i(O.value.answers[e.key]||`—`),1)]))),128)),(n(!0),S(b,null,c(M.value,([e,t])=>(n(),S(`div`,{key:e},[x(`dt`,null,i(e),1),x(`dd`,null,i(t||`—`),1)]))),128))])]),x(`section`,null,[t[20]||=x(`h2`,null,`處理資訊`,-1),x(`dl`,null,[x(`div`,null,[t[17]||=x(`dt`,null,`指派人員`,-1),x(`dd`,null,i(O.value.assignedTo||`尚未指派`),1)]),x(`div`,null,[t[18]||=x(`dt`,null,`指派時間`,-1),x(`dd`,null,i(l(O.value.assignedAt)),1)]),x(`div`,null,[t[19]||=x(`dt`,null,`隱私同意時間`,-1),x(`dd`,null,i(l(O.value.privacyAcceptedAt)),1)])]),g(f(F),{label:`指派人員（規劃中）`,icon:`pi pi-user-plus`,disabled:``,title:`人員指派將於後續版本開放`})])])):_(``,!0)]),_:1},8,[`visible`])]))}}),[[`__scopeId`,`data-v-60307058`]]);export{me as default};
